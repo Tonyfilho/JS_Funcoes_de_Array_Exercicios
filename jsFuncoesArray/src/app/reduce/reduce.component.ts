ac51430e1c197aef2ed4ef24a0032985ee8646db
unknown <oficinadosdrones@gmail.com>
2022-02-10 17:24:29 +0000

    every

--- jsFuncoesArray/src/app/reduce/reduce.component.ts
+++ jsFuncoesArray/src/app/reduce/reduce.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reduce',
   templateUrl: './reduce.component.html',
-  
 })
 export class ReduceComponent implements OnInit {
+  /**
+   * Reduce()
+   */
+  rockets = [
+    { country: 'russia', launches: 32 },
+    { country: 'us', launches: 23 },
+    { country: 'china', launches: 16 },
+    {country: 'europe', launches: 7},
+    {country: 'india', launches: 4},
+    {country: 'japan', launches: 3}
+  ];
+  constructor() {
+    const lancamentos = this.rockets.reduce((acc, array) => {
+    return  acc  = acc + array.launches;
+      
+    }, 0);
 
-  constructor() { }
-
-  ngOnInit(): void {
+    console.group('Usando Reduce para somar um valor de um OBJETO, total de Lançamento é :');
+    console.table(lancamentos);
+    console.groupEnd();
   }
 
+  ngOnInit(): void {}
 }
